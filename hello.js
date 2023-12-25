7f2f81a866eb8c932f66c7e5f0b0559b3e8880cb
shaonkislo2 <shaon6340@gmail.com>
2023-12-25 00:43:52 -0800

    first commit

---
+++ hello.js
@@ -0,0 +1,11 @@
+console.log(100);
+var a = 155;
+console.log(a);
+var today = Date();
+console.log(today);
+var total = .1+.2;
+console.log(total);
+console.log(total.toFixed(1));
+console.log(parseFloat(total.toFixed(1)));
+// console.log(parseFloat(total.toFixed(5)));
+
